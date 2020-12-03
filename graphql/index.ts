8bda9adb7418d1fb79a9aabc5ed73fcb8e405ee5
noaione <noaione0809@gmail.com>
2020-12-03 23:09:05 +0700

    gql: move dataSource init to here

--- graphql/index.ts
+++ graphql/index.ts
@@ -1,41 +1,48 @@
-import {  ApolloServer, gql } from "apollo-server-express";
+import { ApolloServer, gql } from "apollo-server-express";
 import { RedisCache } from "apollo-server-cache-redis";
-import { vtbAPISchema } from "./schemes";
+import { VTAPIv2 } from "./schemas";
 
 import Resolvers = require("./resolver");
 import { is_none } from "../utils/swissknife";
-
-const vtbapiDefs = gql(vtbAPISchema);
+import { NijiTubeDB, VTubersDB } from "../dbconn";
+import { BiliBili } from "./datasources/bilibili";
+import { YoutubeChannel, YoutubeLive } from "./datasources/youtube";
+import { TwitcastingLive, TwitcastingChannel } from "./datasources/twitcasting";
+import { TwitchLive, TwitchChannel } from "./datasources/twitch";
 
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 const REDIS_HOST = process.env.REDIS_HOST;
 const REDIS_PORT = parseInt(process.env.REDIS_PORT);
-if (is_none(REDIS_PASSWORD)) {
-    var cacheServers = new RedisCache({
-        host: is_none(REDIS_HOST) ? "127.0.0.1" : REDIS_HOST,
-        port: isNaN(REDIS_PORT) ? 6379 : REDIS_PORT
-    })
-} else {
-    var cacheServers = new RedisCache({
-        host: is_none(REDIS_HOST) ? "127.0.0.1" : REDIS_HOST,
-        port: isNaN(REDIS_PORT) ? 6379 : REDIS_PORT,
-        password: REDIS_PASSWORD,
-    })
-}
+// if (is_none(REDIS_PASSWORD)) {
+//     var cacheServers = new RedisCache({
+//         host: is_none(REDIS_HOST) ? "127.0.0.1" : REDIS_HOST,
+//         port: isNaN(REDIS_PORT) ? 6379 : REDIS_PORT
+//     })
+// } else {
+//     var cacheServers = new RedisCache({
+//         host: is_none(REDIS_HOST) ? "127.0.0.1" : REDIS_HOST,
+//         port: isNaN(REDIS_PORT) ? 6379 : REDIS_PORT,
+//         password: REDIS_PASSWORD,
+//     })
+// }
 
 const server = new ApolloServer({
-    typeDefs: vtbapiDefs,
+    typeDefs: VTAPIv2,
     resolvers: Resolvers.resolvers,
-    cache: cacheServers,
+    // cache: cacheServers,
+    tracing: true,
     dataSources: () => ({
-        youtubeLive: Resolvers.YoutubeLiveSources,
-        youtubeChannels: Resolvers.YoutubeChannelsSources,
-        nijitubeLive: Resolvers.NijitubeLiveSources,
-        nijitubeChannels: Resolvers.NijitubeChannelsSources,
-        twitchLive: Resolvers.TwitchLiveSources,
-        twitchChannels: Resolvers.TwitchChannelsSources,
-        twitcastingLive: Resolvers.TwitcastingLiveSources,
-        twitcastingChannels: Resolvers.TwitcastingChannelsSources
+        holobili: new BiliBili(VTubersDB.db.collection("hololive_data")),
+        nijibili: new BiliBili(VTubersDB.db.collection("nijisanji_data")),
+        otherbili: new BiliBili(VTubersDB.db.collection("otherbili_data")),
+        youtubeLive: new YoutubeLive(VTubersDB.db.collection("yt_other_livedata")),
+        youtubeChannels: new YoutubeChannel(VTubersDB.db.collection("yt_other_channels")),
+        nijitubeLive: new YoutubeLive(NijiTubeDB.db.collection("nijitube_live")),
+        nijitubeChannels: new YoutubeChannel(NijiTubeDB.db.collection("nijitube_channels")),
+        twitchLive: new TwitchLive(VTubersDB.db.collection("twitch_data")),
+        twitchChannels: new TwitchChannel(VTubersDB.db.collection("twitch_channels")),
+        twitcastingLive: new TwitcastingLive(VTubersDB.db.collection("twitcasting_data")),
+        twitcastingChannels: new TwitcastingChannel(VTubersDB.db.collection("twitcasting_channels")),
     })
 })
 
