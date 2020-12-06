03325084ae062c6c1c79b43ec5b99ecd565847c0
noaione <noaione0809@gmail.com>
2020-12-06 12:11:29 +0700

    saucefinder: add description to schemas

--- graphql/schemas/saucefinder.ts
+++ graphql/schemas/saucefinder.ts
@@ -21,21 +21,38 @@ export const SimilarityScalar = new GraphQLScalarType({
 })
 
 export const SauceAPIGQL = gql`
+    """
+    A JSON formatted (key-value dictionary)
+    """
     scalar JSON
 
+    """
+    Similarity for the match, might be a integer or float.
+    """
     scalar Similarity
 
+    """
+    Possible matching image sauce
+    """
     type SauceObject {
+        "The image name, dependes on the source"
         title: String!
+        "The source url"
         source: String!
+        "How similar the match are"
         confidence: Similarity!
+        "The thumbnail for the match"
         thumbnail: String!
+        "Extra info that might be omitted, for example: Episode and time of the image"
         extra_info: JSON
+        "Indexer used for the image"
         indexer: String
     }
 
     type SauceResource {
+        "Total matches"
         _total: Int!
+        "Collection or a list of closest-matching sauce"
         items: [SauceObject!]
     }
 
