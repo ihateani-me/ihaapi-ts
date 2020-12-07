3409b57d65e7c81ac963d7246c9723a9101ea9c7
noaione <noaione0809@gmail.com>
2020-12-07 14:52:19 +0700

    resolvers: create multi resolver mapping

--- graphql/resolvers/index.ts
+++ graphql/resolvers/index.ts
@@ -1,2 +1,25 @@
+import _ from "lodash";
+
+import { IResolvers } from "apollo-server-express";
+import { SauceGQLResoler } from "./saucefinder";
+import { VTAPIv2Resolvers } from "./vtapi";
+
 export * from "./vtapi";
 export * from "./saucefinder";
+
+let multiResolvers: IResolvers = {
+    Query: {
+        vtuber: () => ({}),
+        sauce: () => ({})
+    },
+    VTuberQuery: VTAPIv2Resolvers["Query"],
+    SauceQuery: SauceGQLResoler["Query"],
+}
+
+let vtresolver = _.omit(VTAPIv2Resolvers, ["Query"]);
+let sauceresolver = _.omit(SauceGQLResoler, ["Query"]);
+_.merge(multiResolvers, vtresolver, sauceresolver);
+
+export {
+    multiResolvers as v2Resolvers
+}
