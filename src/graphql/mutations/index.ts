a496e50fc7dbc5d32245e23c5e1f5a0d36c19eba
noaione <noaione0809@gmail.com>
2021-04-13 04:13:07 +0700

    gql(mut): use partial for ttv and mildom

--- src/graphql/mutations/index.ts
+++ src/graphql/mutations/index.ts
@@ -1,3 +1,9 @@
+import { partialRight } from "lodash";
+
+import config from "../../config";
+import { is_none } from "../../utils/swissknife";
+import { MildomAPI, TwitchHelix } from "./vtuber/helper";
+
 import {
     mildomChannelsDataset,
     ttvChannelDataset,
@@ -5,11 +11,20 @@ import {
     youtubeChannelDataset,
 } from "./vtuber";
 
+const TTVConfig = config.vtapi.twitch;
+let ttvAPIHandler: TwitchHelix;
+if (!is_none(TTVConfig)) {
+    if (!is_none(TTVConfig.client) && !is_none(TTVConfig.secret)) {
+        ttvAPIHandler = new TwitchHelix(TTVConfig.client, TTVConfig.secret);
+    }
+}
+const MILDOMHANDLER = new MildomAPI();
+
 const VTuberMutation = {
     // @ts-ignore
-    twitch: ttvChannelDataset,
+    twitch: partialRight(ttvChannelDataset, ttvAPIHandler),
     twitcasting: twcastChannelsDataset,
-    mildom: mildomChannelsDataset,
+    mildom: partialRight(mildomChannelsDataset, MILDOMHANDLER),
     youtube: youtubeChannelDataset,
 };
 
