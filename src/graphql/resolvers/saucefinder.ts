37f9ef8acec77c171ec602c4f5b87e773cd277c5
noaione <noaione0809@gmail.com>
2021-05-03 17:41:25 +0700

    gql(sauce): handle request error nicely IQDB still not going back

--- src/graphql/resolvers/saucefinder.ts
+++ src/graphql/resolvers/saucefinder.ts
@@ -10,6 +10,7 @@ import { SauceRESTDataSources } from "../datasources";
 import { CustomRedisCache } from "../caches/redis";
 import { getValueFromKey, hasKey } from "../../utils/swissknife";
 import { ASCII2D } from "../../utils/saucefinder";
+import { logger as MainLogger } from "../../utils/logger";
 
 interface SauceContext {
     req: express.Request;
@@ -23,21 +24,42 @@ export const SauceGQLResoler: IResolvers = {
     Similarity: SimilarityScalar,
     Query: {
         saucenao: async (_s, args: SauceNAOParams, ctx: SauceContext, _i): Promise<SauceResource> => {
-            const total_items = await ctx.dataSources.saucenao.getSauce(args.url, args);
-            if (!Array.isArray(total_items) && hasKey(total_items, "error")) {
-                throw new Error(total_items["error"]);
+            const logger = MainLogger.child({ cls: "SauceGQL", fn: "SauceNAO" });
+            try {
+                const total_items = await ctx.dataSources.saucenao.getSauce(args.url, args);
+                if (!Array.isArray(total_items) && hasKey(total_items, "error")) {
+                    throw new Error(total_items["error"]);
+                }
+                // @ts-ignore
+                return { _total: total_items.length, items: total_items };
+            } catch (e) {
+                logger.error(`Failed to reoslve SauceNAO data, reason: ${e.toString()}`);
+                console.error(e);
+                return { _total: 0, items: [] };
             }
-            // @ts-ignore
-            return { _total: total_items.length, items: total_items };
         },
         iqdb: async (_s, args: IQDBParams, ctx: SauceContext, _i): Promise<SauceResource> => {
-            const total_items = await ctx.dataSources.iqdb.getSauce(args.url, args);
-            return { _total: total_items.length, items: total_items };
+            const logger = MainLogger.child({ cls: "SauceGQL", fn: "IQDB" });
+            try {
+                const total_items = await ctx.dataSources.iqdb.getSauce(args.url, args);
+                return { _total: total_items.length, items: total_items };
+            } catch (e) {
+                logger.error(`Failed to reoslve IQDB data, reason: ${e.toString()}`);
+                console.error(e);
+                return { _total: 0, items: [] };
+            }
         },
         ascii2d: async (_s, args: ASCII2DParams, ctx: SauceContext, _i): Promise<SauceResource> => {
-            const A2DAPI = new ASCII2D(getValueFromKey(args, "limit", 2) as number);
-            const total_items = await A2DAPI.getSauce(args.url);
-            return { _total: total_items.length, items: total_items };
+            const logger = MainLogger.child({ cls: "SauceGQL", fn: "ASCII2D" });
+            try {
+                const A2DAPI = new ASCII2D(getValueFromKey(args, "limit", 2) as number);
+                const total_items = await A2DAPI.getSauce(args.url);
+                return { _total: total_items.length, items: total_items };
+            } catch (e) {
+                logger.error(`Failed to reoslve ASCII2D data, reason: ${e.toString()}`);
+                console.error(e);
+                return { _total: 0, items: [] };
+            }
         },
     },
 };
